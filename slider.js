13143917f6e4a0b878b7c430b709bd802b2ec451
ayanawye <kaichybekovaayana@gmail.com>
2023-07-04 20:55:37 +0600

    change style

--- slider.js
+++ slider.js
@@ -1,9 +1,11 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
 let slider = document.querySelector('.swiper-container');
+
 let mySwiper = new Swiper(slider, {
 	slidesPerView: 'auto',
 	spaceBetween:   30,
 })
+
 let slider2 = document.querySelector('.swiper-container2');
 let mySwiper2 = new Swiper(slider2, {
 	slidesPerView: 'auto',
